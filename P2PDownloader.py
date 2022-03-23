1bba407c98cda0e82a04673e1523918b8a99b618
Paul <xiuquanshi@umass.edu>
2022-03-23 17:06:26 -0400

    update

--- P2PDownloader.py
+++ P2PDownloader.py
@@ -1,1 +1,14 @@
-import os
+from socket import *
+import sys
+
+serverName = str(sys.argv[2])
+serverPort = int(sys.argv[3])
+torrent_name = str(sys.argv[1])
+get_command = "GET " + torrent_name + "\n"
+
+
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+clientSocket.sendto(get_command.encode(), (serverName, serverPort))
+test = clientSocket.recvfrom(2048)
+print(type(test))
+print(test)
