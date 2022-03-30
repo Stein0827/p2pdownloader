5f66acc241240943d86eecbfe0e33935477cf0be
Stein0827 <shixiuquanpaul@gmail.com>
2022-03-30 12:30:29 -0400

    finished

--- P2PDownloader.py
+++ P2PDownloader.py
@@ -18,18 +18,17 @@ class UDP_socket:
       self.socket.sendto(self.get_command.encode(), (self.serverName, self.serverPort))
    
    def recv(self):
-      self.socket.settimeout(3)
+      self.socket.settimeout(0.5)
       try:
-         data = self.socket.recvfrom(500)
+         data = self.socket.recvfrom(200)
          if data != None:
-            print("this is the data inside udp recv: ", data)
             return data
          else:
             self.send()
-            self.recv()
+            return self.recv()
       except timeout:
          self.send()
-         self.recv()
+         return self.recv()
 
    def get_ip(self):
       return gethostbyname(self.serverName)
@@ -61,9 +60,12 @@ class TCP_socket:
          get_command = "GET " + self.file_name + ":" + str(block_number) + "\n"
       else:
          get_command = "GET " + self.file_name + ":*" + "\n"
-
-      self.socket.connect((self.serverName, self.serverPort))
-      self.socket.send(get_command.encode())
+      self.socket.settimeout(2)
+      try:
+         self.socket.connect((self.serverName, self.serverPort))
+         self.socket.send(get_command.encode())
+      except timeout:
+         return self.get_block(block_number)
 
    #TODO PRIORITY: Handle TCP Server disconnection while downloading
    def recv(self, block_number=None):
@@ -71,14 +73,14 @@ class TCP_socket:
 
       #UNDESTANDING: Collect header file until body of bytes starts
       while b'\n\n' not in data:
-         self.socket.settimeout(5)
+         self.socket.settimeout(2)
          try:
             data += self.socket.recv(512)
          except timeout:
             self.socket.close()
             self.socket = self.socket_init()
             self.get_block(block_number)
-            self.recv()
+            return self.recv()
       
       data_tok = data.split(b"\n")
       data_length = int(data_tok[2][18:])
@@ -89,7 +91,7 @@ class TCP_socket:
       data_length -= len(block_data)
 
       while data_length > 0:
-         self.socket.settimeout(5)
+         self.socket.settimeout(2)
          try:
             data = self.socket.recv(512)
             block_data += data
@@ -98,7 +100,7 @@ class TCP_socket:
             self.socket.close()
             self.socket = self.socket_init()
             self.get_block(block_number)
-            self.recv()
+            return self.recv()
       
 
       return (block_data, data_offset)
@@ -109,22 +111,23 @@ class TCP_socket:
 
 def tcp_thread_requests(cblocks_lock, mblocks_lock, ap_lock, file_name, peer, num_blocks):
    global collected_blocks, missing_blocks, active_peers, peers_set
-   
+
    # udp_socket = UDP_socket()
    # udp_socket.send()
    # msg = udp_socket.recv()
-   # print("this is the msg: ", msg)
    # udp_socket.close()
    
    # if msg != None:
    #    num_blocks, file_size, temp_peers = udp_socket.get_metadata(msg[0])
-   #    print("In threads: ", temp_peers)
    #    ap_lock.acquire()
    #    for peer in temp_peers:
-   #       peers_set.add(peer)
+   #       if peer not in active_peers:
+   #          peers_set.add(peer)
    #    ap_lock.release()
-      
-   while len(collected_blocks) != num_blocks:
+   
+   
+
+   while len(collected_blocks) != num_blocks and len(missing_blocks) != 0:
       mblocks_lock.acquire()
       if missing_blocks:
          curr_block = missing_blocks.pop()
@@ -141,16 +144,22 @@ def tcp_thread_requests(cblocks_lock, mblocks_lock, ap_lock, file_name, peer, nu
 
       cblocks_lock.acquire()
       collected_blocks[curr_block] = block_data
+      # print("***COLLECTED_BLOCKS***", len(collected_blocks), threading.get_ident())
+      blocks = len(collected_blocks)
       cblocks_lock.release()
 
+   # print("hello world")
    tcp_socket.close()
 
 
 def block_to_image(collected_blocks: dict, image_name: str):
    image_file = open(image_name, 'wb')
+   image_start = timer()
    for block in sorted(collected_blocks):
       image_file.write(collected_blocks[block])
    image_file.close()
+   image_end = timer()
+   print("image writing time: ", image_end-image_start)
 
 
 def main():
@@ -169,35 +178,42 @@ def main():
    #    missing_blocks = [*range(num_blocks)]
    #    for elem in temp_set:
    #       peers_set.add(elem)
-   
    udp_socket = UDP_socket()
-   udp_socket.send()
-   msg = udp_socket.recv()
-   print("msg in main: ", type(msg))
-   while msg == None:
-      udp_socket.send()
-      msg = udp_socket.recv()
-
-   num_blocks, file_size, temp_set = udp_socket.get_metadata(msg[0])
-   missing_blocks = [*range(num_blocks)]
-   for elem in temp_set:
-      peers_set.add(elem)
    
+   sock_start = timer()
+   for _ in range(3):
+      udp_socket.send()
+
+      recv_start = timer()
+      msg = udp_socket.recv()
+      recv_end = timer()
+      print("RECV TIMER: ", recv_end-recv_start )
+
+      num_blocks, file_size, temp_set = udp_socket.get_metadata(msg[0])
+      missing_blocks = [*range(num_blocks)]
+      for elem in temp_set:
+         peers_set.add(elem)
+   sock_end = timer()
+   print("SOCKETS TIMER: ", sock_end-sock_start)
    udp_socket.close()
 
+   for _ in range(len(peers_set) - 5):
+      peers_set.pop()
 
    thread_arr = []
-   while peers_set:
+   while peers_set and len(collected_blocks) != num_blocks:
       popped_peer = peers_set.pop()
       if popped_peer not in active_peers:
          active_peers.add(popped_peer)
          curr_thread = threading.Thread(target=tcp_thread_requests, args=(cblocks_lock, mblocks_lock, ap_lock, udp_socket.file_name, popped_peer, num_blocks))
          thread_arr.append(curr_thread)
          curr_thread.start()
-
+   # print("out of while")
    for thread in thread_arr:
       thread.join()
-      
+   
+   # print("bananas")
+   
    block_to_image(collected_blocks, udp_socket.file_name)
    end = timer()
    print("download time: ", end-start)
---
+++ help.py
@@ -0,0 +1,3 @@
+set1 = {("hello", 123), ("bruh", 123)}
+set2 = [("bruh", 234), ("hello", 234)]
+print(set1.pop())
